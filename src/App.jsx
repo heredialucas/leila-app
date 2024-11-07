5b3d5611e5f0511f0b5d9021707653ae9230bc22
heredialucas <heredialucasfac22@gmail.com>
2024-11-08 00:25:45 +0100

    color change

--- src/App.jsx
+++ src/App.jsx
@@ -22,90 +22,7 @@ import {
 } from "@mui/material";
 import DeleteIcon from "@mui/icons-material/Delete";
 import CommentIcon from "@mui/icons-material/Comment";
-
-const menu = [
-  // Cold Mezze Section
-  { position: 12, name: "Hummus" },
-  { position: 13, name: "Hummus with sun-dried tomatoes" },
-  { position: 14, name: "Hummus with Pesto sauce" },
-  { position: 15, name: "Hummus tartufo" },
-  { position: 16, name: "Maghmour" },
-  { position: 17, name: "Baba Ghanouj" },
-  { position: 18, name: "Muhammara" },
-  { position: 19, name: "Labneh" },
-  { position: 20, name: "Labneh Bi Thoum" },
-  { position: 21, name: "Labneh W Zaatar" },
-  { position: 22, name: "Makdous" },
-  { position: 23, name: "Shanklish" },
-  { position: 24, name: "Warak Enab - Stuffed grape leaves" },
-  { position: 25, name: "Rahib" },
-  // Hot Mezze Section
-  { position: 26, name: "Hummus Lahmeh" },
-  { position: 27, name: "Hummus Awarma" },
-  { position: 28, name: "Hummus with Chicken liver" },
-  { position: 29, name: "Batata Kouzbara" },
-  { position: 30, name: "Batata Harra" },
-  { position: 31, name: "Fatteh hummus" },
-  { position: 32, name: "Fatteh Batenjen" },
-  { position: 33, name: "Fatteh with Meat" },
-  { position: 34, name: "Chicken liver" },
-  { position: 35, name: "Kebbeh 4 pcs" },
-  { position: 36, name: "Cheese Rakakat 4 pcs" },
-  { position: 37, name: "Sambousik meat 4 pcs" },
-  { position: 38, name: "Falafel with Taratour Sauce" },
-  { position: 39, name: "Fatayer 4 pcs" },
-  { position: 40, name: "Makanek" },
-  { position: 41, name: "Acquadella" },
-  { position: 42, name: "Fried Cauliflower" },
-
-  // Grill Section
-  { position: 43, name: "Chicken Tawouk" },
-  { position: 44, name: "Kafta" },
-  { position: 45, name: "Grilled Lamb" },
-  { position: 46, name: "Kastaleta" },
-  { position: 47, name: "Mix grill plate" },
-  { position: 48, name: "Double Mix grill plate" },
-  { position: 49, name: "Shawarma meat plate" },
-  { position: 50, name: "Arayes" },
-
-  // Oven Section
-  { position: 51, name: "Sfíha" },
-  { position: 52, name: "Manakish Zaatar" },
-  { position: 53, name: "Manakish jebne" },
-  { position: 54, name: "Fokhara Djej" },
-  { position: 55, name: "Fokhara Lahme" },
-  { position: 56, name: "Fokhara Khodra" },
-
-  // Salads Section
-  { position: 57, name: "Tabbouleh" },
-  { position: 58, name: "Tabbouleh Quinoa" },
-  { position: 59, name: "Fattouche" },
-  { position: 60, name: "Rokka dates salad" },
-
-  // Soups Section
-  { position: 61, name: "Lentil Soup" },
-
-  // Siders Section
-  { position: 62, name: "French fries" },
-  { position: 63, name: "Rice" },
-  { position: 64, name: "Gluten free Bread" },
-  { position: 65, name: "Lebanese Bread (2 pcs)" },
-  { position: 66, name: "Mixed pickles" },
-  { position: 67, name: "Garlic Cream" },
-
-  // Desserts Section
-  { position: 68, name: "Baklava" },
-  { position: 69, name: "Mohallabieyeh" },
-  { position: 70, name: "Halawet El Jibn" },
-  { position: 71, name: "Mafroukeh" },
-  { position: 72, name: "Kunafa" },
-
-  // Kids Menu
-  {
-    position: 73,
-    name: "Chicken Nuggets with fries, ketchup & glass of juice (200ml)",
-  },
-];
+import { menu } from "./menu";
 
 export default function App() {
   const [selectedItems, setSelectedItems] = useState([]);
@@ -258,27 +175,15 @@ export default function App() {
                 button
                 onClick={() => handleSelect(item)}
                 selected={selectedItem?.position === item.position}
+                style={{
+                  backgroundColor: `${item.color}`,
+                  fontWeight: "bolder",
+                }}
               >
                 <ListItemText primary={`${item.position} - ${item.name}`} />
               </ListItem>
             ))}
           </List>
-
-          <Box
-            sx={{
-              display: "flex",
-              justifyContent: "flex-end",
-              marginBottom: 2,
-            }}
-          >
-            <Button
-              variant="outlined"
-              startIcon={<CommentIcon />}
-              onClick={() => handleOpenCommentDialog(null)}
-            >
-              Comments
-            </Button>
-          </Box>
 
           <TableContainer component={Paper}>
             <Table>
@@ -293,7 +198,13 @@ export default function App() {
               </TableHead>
               <TableBody>
                 {selectedItems.map((item) => (
-                  <TableRow key={item.position}>
+                  <TableRow
+                    style={{
+                      backgroundColor: `${item.color}`,
+                      fontWeight: "bolder",
+                    }}
+                    key={item.position}
+                  >
                     <TableCell>{item.position}</TableCell>
                     <TableCell>{item.name}</TableCell>
                     <TableCell>
